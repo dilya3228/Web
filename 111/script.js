c3634c6bfd033ed25a782975601ff74337c31d1a
Delirious <alex.zik3@mail.ru>
2022-03-30 23:46:02 +0300

    111

---
+++ 111/script.js
@@ -0,0 +1,20 @@
+let phrases = [
+    'отправить другу смешную гифку',
+    'посмотреть скидки на авиабилеты',
+    'разобраться, о чём поют рэперы',
+    'Юрий Дудь',
+    'расставить книги на полке по цвету',
+    'читать про зарплаты в Сан-Франциско'
+  ];
+  
+  function getRandomElement(arr) {
+    let randIndex = Math.floor(Math.random() * arr.length);
+    return arr[randIndex];
+  }
+  
+  let button = document.querySelector('.button');
+  let advice = document.querySelector('.advice');
+  let phrase = document.querySelector('.phrase');
+  let image = document.querySelector('.image');
+  
+  phrase.textContent = getRandomElement(phrases);
